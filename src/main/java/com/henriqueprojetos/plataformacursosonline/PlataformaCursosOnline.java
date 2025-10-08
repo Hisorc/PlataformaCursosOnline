c7626f09819eccf26a66e3e1a2b4d0d8c96f45ae
hisorc <shimmoo2001@gmail.com>
2025-10-08 20:51:43 -0300

    first commit

---
+++ src/main/java/com/henriqueprojetos/plataformacursosonline/PlataformaCursosOnline.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.henriqueprojetos.plataformacursosonline;
+
+/**
+ *
+ * @author Henri
+ */
+public class PlataformaCursosOnline {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
